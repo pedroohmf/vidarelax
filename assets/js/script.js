fbcaeb35b76997f987e6381dd7b34e7aec6d7c72
Pedro Pc Prefeitura <pedrohfm2012@yahoo.com.br>
2025-01-17 12:15:08 -0300

    Criando footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,3 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-prev',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
 
 $(document).ready(function() {
   $.scrollify({
